3de1c042d5968b1473608f6adcf40921aaf88ff8
raj-raghuwanshi <rajveerraghuw@gmail.com>
2018-12-21 19:53:31 -0800

    Hello there past me you are epick too
    epick!!!!

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180/Autonomous_Depot.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180/Autonomous_Depot.java
@@ -16,6 +16,9 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Autonomous(name="Autonomous_Depot", group="autonomusGroup1")
 public class Autonomous_Depot extends LinearOpMode {
 
+
+    //Hello future me you are epick
+
     private RoboNavigator robotNavigator;
     private RoboLander lander;
     private RoboGrabber grabber;
