6d58a0f086c369d91749b65f27286f7f6c77d457
Rohan <40905346+Rohan1215@users.noreply.github.com>
2019-01-12 12:07:11 -0800

    Update TestGrabber.java

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180/TestGrabber.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180/TestGrabber.java
@@ -2,14 +2,14 @@ package org.firstinspires.ftc.team13180;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
 
 @TeleOp(name="TestGrabber", group ="manualmode")
 public class TestGrabber extends LinearOpMode{
 
     private RoboGrabber grabber;
 
-    static final double SPEED = 0.5;
+    static final double SPEED = 1;
 
     @Override
     public void runOpMode() {
@@ -27,11 +27,18 @@ public class TestGrabber extends LinearOpMode{
             else if (gamepad1.b) {
                 grabber.moveGrabberDown(SPEED);
             }
-            else if (gamepad1.x) {
+            else {
+                grabber.stopGrabber();
+            }
+
+            if (gamepad1.x) {
                 grabber.spinIn();
             }
             else if (gamepad1.y) {
                 grabber.spinOut();
+            }
+            else {
+                grabber.stopSpin();
             }
 
             }
